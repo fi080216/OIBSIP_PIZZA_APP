617ffa978ed1da09144a34a7a15c5dd291718ffc
Faisal Imam <fi492055@gmail.com>
2024-05-23 16:58:01 +0530

    cart page

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const ejs = require('ejs');
-const path = require('path');
-const expressLayout = require('express-ejs-layouts');
+const ejs = require("ejs");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
 
-
-app.get("/",  (req,res) => {
-res.render('home');
-});
+app.use(express.static('public'))
 
 
 
 
 app.use(expressLayout);
-app.set('views', path.join(__dirname, '/resources/views'));
-app.set('view engine' , 'ejs');
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
 
 
+app.get("/", (req, res) => {
+  res.render('home');
+});
 
+app.get("/cart", (req, res)=>{
+  res.render('customers/cart');
+})
 
+app.get('/login', (req,res)=>{
+  res.render('path/login')
+})
 
+app.get('/register', (req,res)=>{
+  res.render('path/register')
+})
 
 app.listen(3000, () => {
-console.log("listening on port 3000");
+  console.log("listening on port 3000");
 });
